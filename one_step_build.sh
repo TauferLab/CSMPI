0bc6b11799fc99bf342fbf9f5e6737ba18f50a30
Dylan Chapp <dchapp@udel.edu>
2019-12-20 10:58:29 -0500

    Remove hardcoded boost/libunwind locations
    Now that boost and libunwind are installed via spack and exposed
    via environment-modules, FindBoost.cmake and FindLibunwind.cmake
    are working as intended.

--- one_step_build.sh
+++ one_step_build.sh
@@ -3,6 +3,5 @@
 export CC=mpicc
 export CXX=mpicxx
 rm -rf $(pwd)/build && mkdir -p $(pwd)/build && cd build
-cmake -DBOOST_ROOT=$HOME/repos/spack/opt/spack/linux-rhel7-power9le/gcc-9.2.0/boost-1.70.0-4yq3t45pazg3zrrek2a2hnt5efglmsmv/ \
-      ..
+cmake ..
 make
